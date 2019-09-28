f43e55702d857499ebdc9b6859c43c2018502914
unknown <krzroz@outlook.com>
2019-09-28 19:22:38 +0200

    Add mul option to menu

--- Calc/Program.cs
+++ Calc/Program.cs
@@ -15,6 +15,7 @@ namespace Calc
                 Console.WriteLine("0. Exit");
                 Console.WriteLine("1. Add");
                 Console.WriteLine("2. Diff");
+                Console.WriteLine("3. Mul");
                 int menu = Convert.ToInt32(Console.ReadLine());
                 if (menu == 0)
                 {
